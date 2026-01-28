74ba7fc21d7df2c288947e5be7c1028916de5955
Mauricio0704 <mausancarr0704@gmail.com>
2026-01-27 23:27:22 -0600

    feat: integrate Tailwind CSS and update courses and professors API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   output: "server",
   adapter: vercel({}),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
